9301a3d1f4841784f74f4cad40d6a8b42495d961
MoeShoumar <mks38@mail.aub.edu>
2023-02-24 19:59:39 +0200

    added the conditions in app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+
 let result = document.getElementById('result')
 let dice1_result = document.getElementById('dice1_result')
-let dice2_result = document.getElementById('dice1_result')
+let dice2_result = document.getElementById('dice2_result')
+dice1_result.innerHTML = Math.floor(Math.random() * 6) + 1;
 
-let generated_number = Math.floor(Math.random() * 6) + 1;
-console.log(generated_number)
+dice2_result.innerHTML = Math.floor(Math.random() * 6) + 1;
+
+// Player 1 win
+if (dice1_result.innerHTML > dice2_result.innerHTML) {
+    result.innerHTML = 'Player-1 Win!'
+}
+
+
+// Player 2 win
+
+if (dice1_result.innerHTML < dice2_result.innerHTML) {
+    result.innerHTML = 'Player-2 Win!'
+}
+
+
+
+// Draw
+if (dice1_result.innerHTML == dice2_result.innerHTML) {
+    result.innerHTML = 'Draw!'
+}
