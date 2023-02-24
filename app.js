0d34b40dd784f3ff236223dda7a09bcd53e236ae
MoeShoumar <mks38@mail.aub.edu>
2023-02-24 18:42:28 +0200

    selected ids in js and created the number function

---
+++ app.js
@@ -0,0 +1,6 @@
+let result = document.getElementById('result')
+let dice1_result = document.getElementById('dice1_result')
+let dice2_result = document.getElementById('dice1_result')
+
+let generated_number = Math.floor(Math.random() * 6) + 1;
+console.log(generated_number)
